c3d40a94356a4e96b1897aa162477ce758cafd7d
NunyaBizness <jmclark801@yahoo.com>
2019-04-09 18:36:03 -0600

    bug fix and decode html entities

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,13 +10,14 @@ var correct = 0
 var incorrect = 0
 var intervalID = ""
 
+// ***********************
 // Get questions using API 
-function loadQuestions(){
+function loadQuestions() {
   var request = new XMLHttpRequest()
   request.open('GET', 'https://opentdb.com/api.php?amount=10&category=11&type=multiple', true)
   //amount has been set into this request to 3 questions so this can be tested. The amount 
   //should be switched to 10 or more when ready for project submission
-  request.onload = function(){
+  request.onload = function () {
     var data = JSON.parse(this.response);
     console.log(data);
     data.results.forEach(question => {
@@ -26,85 +27,97 @@ function loadQuestions(){
   request.send();
 }
 
-// Timer Functions
-function decrementCounter(){
-  if (time > 0){
-    time--
-    $('#countdown').text(time)
-  } else {
-    incorrectAnswerHandler();
-  } 
-}
-
-document.getElementById("button-begin").addEventListener('click', function(){
+document.getElementById("button-begin").addEventListener('click', function () {
   playGame()
 });
 
-function playGame(){
+function playGame() {
   intervalID = setInterval(decrementCounter, 1000)
   beginGame();
-  displayQuestion(); 
+  displayQuestion();
 }
 
-function beginGame(){
+function beginGame() {
   $('#button-begin').addClass('hide');
   correct = 0
-  incorrect = 0 
+  incorrect = 0
   time = secondsPerQuestion
+}
+
+// The API results include 'HTML entities that need to be decoded. 
+// Turns out that's not super easy to do. Thus this function.
+// Compare the results in console.log to the decoded results
+function decodeResponse(response) {
+  var responseToDecode = response;
+  var encodedResponse = encodeURI(responseToDecode);
+  var decodedResponse = decodeURI(encodedResponse);
+  return decodedResponse
 }
 
 function displayQuestion() {
   var length = questions[questionCounter].incorrect_answers.length + 1;
-  var randomNumber = (Math.floor(Math.random() * length)) 
+  var randomNumber = (Math.floor(Math.random() * length))
   var answersArray = questions[questionCounter].incorrect_answers
   var correctAnswer = questions[questionCounter].correct_answer
+  console.log("Correct Answer = ", correctAnswer)
   answersArray.splice(randomNumber, 0, correctAnswer)
 
-  $('#question').text(questions[questionCounter].question).removeClass("hide").addClass("show");
-  for (var i=0; i< answersArray.length; i ++){
-    $('#answer'+ (i+1)).html(answersArray[i]).removeClass("hide").addClass("show answer")
+  $('#question').html(decodeResponse(questions[questionCounter].question)).removeClass("hide").addClass("show");
+  for (var i = 0; i < answersArray.length; i++) {
+    $('#answer' + (i + 1)).html(decodeResponse(answersArray[i])).removeClass("hide").addClass("show answer")
   }
-
-  // ************************
-  // For the html entity, try .html(thing to insert).text()
   console.log(answersArray);
-
-  $(".answer").on('click', function(){
-    if (this.textContent === questions[questionCounter].correct_answer){
-      correctAnswerHandler()
-    } else {
-      incorrectAnswerHandler()
-    }
-  })
 }
 
-function correctAnswerHandler(){
-  correct++
-  nextQuestion()
+function correctAnswerHandler() {
+  correct++;
+  // $(this).addClass('correct-answer')
+  // setInterval(nextQuestion, 500)
+  console.log("You guessed right!")
+  nextQuestion();
 }
 
-function incorrectAnswerHandler(){
+function incorrectAnswerHandler() {
   incorrect++;
   nextQuestion();
 }
 
-function endGame(){
+function endGame() {
   clearInterval(intervalID)
   console.log("The game is over, play again?")
   console.log("You got: " + incorrect + " wrong! USUX")
   console.log("You got: " + correct + " correct")
 }
 
-function nextQuestion(){
-  if (questionCounter < questions.length -1) {
+function nextQuestion() {
+  if (questionCounter < questions.length - 1) {
     time = secondsPerQuestion;
-    questionCounter++ 
+    questionCounter++
+    console.log("in next Question")
     displayQuestion();
   } else {
     endGame()
-  } 
+  }
 }
+
 loadQuestions();
 
+// *****************
+// Utility Functions
+function decrementCounter() {
+  if (time > 0) {
+    time--
+    $('#countdown').text(time)
+  } else {
+    incorrectAnswerHandler();
+  }
+}
 
-
+// Main Click Handler
+$(".answer").on('click', function () {
+  if (this.textContent === questions[questionCounter].correct_answer) {
+    correctAnswerHandler()
+  } else {
+    incorrectAnswerHandler()
+  }
+})
