68512813d40e7ed0e44ea1de1412573d8800732e
NunyaBizness <jmclark801@yahoo.com>
2019-04-11 14:01:33 -0600

    Assignment complete

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,10 +7,14 @@ var answers = []
 var correct = 0
 var incorrect = 0
 var intervalID = ""
+var audio = new Audio('./assets/Sounds/WitchCackle.mp3');
+
+
 
 // ***********************
 // Get questions using API 
 function loadQuestions() {
+  questions = []
   var request = new XMLHttpRequest()
   request.open('GET', 'https://opentdb.com/api.php?amount=10&category=11&type=multiple', true)
   request.onload = function () {
@@ -34,35 +38,36 @@ document.getElementById("button-begin").addEventListener('click', function () {
 });
 
 function playGame() {
+  time = secondsPerQuestion
   intervalID = setInterval(decrementCounter, 1000)
   $('#button-begin').addClass('hide')
-  $('#results').addClass('hide')
-  $('#content').addClass('show')
+  $('#results').addClass('hide').css('display', 'none')
+  $('#content').addClass('show').css('display', 'flex')
   correct = 0
   incorrect = 0
-  time = secondsPerQuestion
   displayQuestion();
 }
-
-
 
 function nextQuestion() {
   if (questionCounter < questions.length - 1) {
     $("#countdown").text(secondsPerQuestion)
     time = secondsPerQuestion;
     questionCounter++
-    console.log("in next Question")
     displayQuestion();
   } else {
     endGame()
   }
 }
 
+// This function gets the array of incorrect answers and randomly
+// inserts the correct answer in that array.
+// It also runs a function to decode html entities that come from the API
 function displayQuestion() {
   var length = questions[questionCounter].incorrect_answers.length + 1;
   var randomNumber = (Math.floor(Math.random() * length))
   var answersArray = questions[questionCounter].incorrect_answers
   var correctAnswer = questions[questionCounter].correct_answer
+  // Correct answer is logged only for demonstration purposes
   console.log("Correct Answer = ", correctAnswer)
   answersArray.splice(randomNumber, 0, correctAnswer)
   $('#countdown').removeClass('hide')
@@ -84,23 +89,24 @@ function incorrectAnswerHandler() {
 }
 
 function endGame() {
+  audio.play();
+  setTimeout(function () {
+    audio.pause();
+  }, 3000);
   clearInterval(intervalID)
   $('#content').css('display', 'none')
-  $('#results').removeClass('hide').addClass('show')
+  $('#results').removeClass('hide').addClass('show').css('display', 'flex')
   $('#results-correct').text("Correct Answers: " + correct)
   $('#results-incorrect').text("Incorrect Answers: " + incorrect)
-  console.log("The game is over, play again?")
-  console.log("You got: " + incorrect + " wrong! USUX")
-  console.log("You got: " + correct + " correct")
 }
 
 function playGameAgain(){
-  
+  loadQuestions()
+  questionCounter = 0
   playGame()
 }
 
-// *****************
-// Utility Functions
+// Used for the timer
 function decrementCounter() {
   if (time > 0) {
     time--
@@ -108,11 +114,15 @@ function decrementCounter() {
   } else {
     incorrectAnswerHandler();
   }
+  if (time < 5) {
+    $('#countdown').css('color', '#A50520')
+  } else {
+    $('#countdown').css('color', '#A4C552')
+  }
 }
 
-// The API results include 'HTML entities that need to be decoded. 
+// The API results include 'HTML entities' that need to be decoded. 
 // Turns out that's not super easy to do. Thus this function.
-// Compare the results in console.log to the decoded results
 function decodeResponse(response) {
   var responseToDecode = response;
   var encodedResponse = encodeURI(responseToDecode);
